7e0c2153266bd839516f7ef5fada25d3f52b0e81
Apollotwin <73161942+Apollotwin@users.noreply.github.com>
2020-11-11 15:34:37 +0100

    Created script Resource
    You can now create Scripteble objects

---
+++ Project 1/Assets/Scripts/Resource/Resource.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor.UI;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Resource : ScriptableObject
+{
+    public Color Color;
+    public int amountPerClick = 1;
+
+    public int Owned {
+        get => PlayerPrefs.GetInt(this.name, 0);
+        set => PlayerPrefs.SetInt(this.name, value);
+    }
+
+    public void Produce()
+    {
+        this.Owned += this.amountPerClick;
+    }
+}
