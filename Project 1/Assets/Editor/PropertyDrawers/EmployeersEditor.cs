ad109599c597ece48316c1a2ad379828a42ef93a
Alex <alex.rynkowski90@gmail.com>
2020-11-12 23:48:52 +0100

    adds colors EmployersUI editor

--- Project 1/Assets/Editor/PropertyDrawers/EmployeersEditor.cs
+++ Project 1/Assets/Editor/PropertyDrawers/EmployeersEditor.cs
@@ -17,7 +17,7 @@ namespace PropertyDrawers
             GUILayout.Label("Resource cost/type", EditorStyles.boldLabel);
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Cost",GUILayout.Width(80));
+            GUILayout.Label("Cost",GUILayout.Width(90));
             value2.purchasable.cost = EditorGUILayout.IntField(value2.purchasable.cost);
             value2.purchasable.resource = (Resource) EditorGUILayout.ObjectField(value2.purchasable.resource, typeof(Resource), true);
             GUILayout.EndHorizontal();
@@ -26,19 +26,25 @@ namespace PropertyDrawers
             GUILayout.Label("Employers owned / text", EditorStyles.boldLabel);
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Hunters",GUILayout.Width(80));
+            var pink = new GUIStyle();
+            pink.normal.textColor = new Color(0.9433962f,0.3604486f,0.8352419f);
+            EditorGUILayout.LabelField("Hunters", pink,GUILayout.Width(90));
             value.ownedHunters = EditorGUILayout.IntField(value.ownedHunters);
             value.ownedHuntersText = (Text) EditorGUILayout.ObjectField(value.ownedHuntersText, typeof(Text), true);
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("Miners",GUILayout.Width(80));
+            var yellow = new GUIStyle();
+            yellow.normal.textColor = Color.yellow;
+            EditorGUILayout.LabelField("Miners", yellow,GUILayout.Width(90));
             value.ownedMiners = EditorGUILayout.IntField(value.ownedMiners);
             value.ownedMinersText = (Text) EditorGUILayout.ObjectField(value.ownedMinersText, typeof(Text), true);
             GUILayout.EndHorizontal();
 
             GUILayout.BeginHorizontal();
-            GUILayout.Label("LumberJacks", GUILayout.Width(80));
+            var brown = new GUIStyle();
+            brown.normal.textColor = new Color(0.5943396f,0.4105989f,0.2775454f);
+            EditorGUILayout.LabelField("Lumber Jacks", brown,GUILayout.Width(90));
             value.ownedLumberJacks = EditorGUILayout.IntField(value.ownedLumberJacks);
             value.ownedLumberJacksText = (Text) EditorGUILayout.ObjectField(value.ownedLumberJacksText, typeof(Text), true);
             GUILayout.EndHorizontal();
