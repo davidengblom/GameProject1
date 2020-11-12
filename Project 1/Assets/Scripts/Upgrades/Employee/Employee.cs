3ef601ae5025c4c219baba08badd9231b3081ae0
Jonas A <jonas.askling@student.forsbergsskola.se>
2020-11-12 13:39:10 +0100

    Created employee script
    Produce resources

---
+++ Project 1/Assets/Scripts/Upgrades/Employee/Employee.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Employee : MonoBehaviour
+{
+    public int productionAmount = 1;
+    public float productionTime = 1f;
+
+    public Text resourceAmountText;
+    public Text resourceTypeText;
+    public Resource resource;
+
+    float elapsedTime;
+
+    private void Start()
+    {
+        UpdateResourceAmountLabel();
+        this.resourceTypeText.text = this.resource.name;
+    }
+
+    void Update()
+    {
+        this.elapsedTime += Time.deltaTime;
+        if (this.elapsedTime >= this.productionTime)
+        {
+            ProduceResource();
+
+            this.elapsedTime -= this.productionTime;
+        }
+        UpdateResourceAmountLabel();
+    }
+
+    void ProduceResource()
+    {
+        resource.Owned += this.productionAmount;
+    }
+
+    
+
+    void UpdateResourceAmountLabel()
+    {
+        this.resourceAmountText.text = this.resource.Owned.ToString();
+    }
+
+    
+}
