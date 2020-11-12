5ef55294b99aed1a9a62e64209e6490f0ca7ceab
David Engblom <kirellen1573@zoho.eu>
2020-11-12 16:38:29 +0100

    Increase resource gained from clicking added

---
+++ Project 1/Assets/Scripts/Upgrades/Resources/ResourceGain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class ResourceGain : MonoBehaviour
+{
+    //To Do List
+    //1. Function to increase resource gain from clicking by x %
+    //2. Function to increase resource gain from employees (automatic) by x %
+
+    public Resource resource;
+    public float percent;
+
+    public void ClickUpgrade()
+    {
+        resource.amountPerClick = (int) (resource.amountPerClick * percent);
+    }
+
+    public void EmployeeUpgrade()
+    {
+            
+    }
+}
