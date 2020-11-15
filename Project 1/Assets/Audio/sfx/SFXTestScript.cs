c8b2650e895dcb0a54db2f6daa9a0d489d87d1c5
Apollotwin <73161942+Apollotwin@users.noreply.github.com>
2020-11-15 20:46:44 +0100

    Added SFXPlayer & tried making the buttons make noises

---
+++ Project 1/Assets/Audio/sfx/SFXTestScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class SFXTestScript : MonoBehaviour {
+    public AudioClip[] FoodSounds;
+    public AudioClip[] StoneSounds;
+    public AudioClip[] LumberSounds;
+
+    public void PlayLumberSounds()
+    {
+        var random = Random.Range(0, LumberSounds.Length);
+        AudioSource audio = GetComponentInParent<AudioSource>();
+        audio.clip = LumberSounds[random];
+        audio.Play();
+        Debug.Log("Playing Sound nr: " + LumberSounds[random].name);
+    }
+    
+    public void PlayFoodSounds()
+    {
+        var random = Random.Range(0, FoodSounds.Length);
+        AudioSource audio = GetComponentInParent<AudioSource>();
+        audio.clip = FoodSounds[random];
+        audio.Play();
+        Debug.Log("Playing Sound nr: " + FoodSounds[random].name);
+    }
+    
+    public void PlayStoneSounds()
+    {
+        var random = Random.Range(0, StoneSounds.Length);
+        AudioSource audio = GetComponentInParent<AudioSource>();
+        audio.clip = StoneSounds[random];
+        audio.Play();
+        Debug.Log("Playing Sound nr: " + StoneSounds[random].name);
+    }
+}
