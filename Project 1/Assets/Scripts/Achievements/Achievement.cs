e6c926e1b1d1e3d661f26067e958b6cbb35db9db
Alex <alex.rynkowski90@gmail.com>
2020-11-16 22:53:29 +0100

    adds scriptable objct for achievement system

---
+++ Project 1/Assets/Scripts/Achievements/Achievement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Achievements
+{
+    [CreateAssetMenu]
+    public class Achievement : ScriptableObject
+    {
+        public int reward = 0;
+        public Resource resourceReward;
+        public AchievementRequirementType achievementRequirementType;
+    }
+
+    public enum AchievementRequirementType
+    {
+        Clicking,
+        AutoClicking,
+        
+    }
+}
