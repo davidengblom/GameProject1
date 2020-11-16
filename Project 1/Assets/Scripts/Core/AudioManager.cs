ad35f717fab336d94d76e3f826a43d067b8607b9
Alex <alex.rynkowski90@gmail.com>
2020-11-16 18:37:04 +0100

    fixes audiomanager. Should now randomize a sound clip

--- Project 1/Assets/Scripts/Core/AudioManager.cs
+++ Project 1/Assets/Scripts/Core/AudioManager.cs
@@ -1,11 +1,10 @@
-﻿
-using UnityEngine;
-[System.Serializable]
+﻿using UnityEngine;
 
+[System.Serializable]
 public class Sound
 {
     public string name;
-    public AudioClip [] clip;
+    public AudioClip[] clip;
 
     [Range(0f, 1f)]
     public float volume = 0.7f;
@@ -17,73 +16,72 @@ public class Sound
     public float randomPitch = 0.1f;
 
 
-
     private AudioSource source;
 
-    public void SetSource (AudioSource _source)
+    public void SetSource(AudioSource _source, int j)
     {
         source = _source;
-        source.clip = clip[0];
+        this.source.clip = this.clip[j];
     }
 
-    public void Play ()
+    public void Play()
     {
         source.volume = volume * (1 + Random.Range(-randomVolume / 2f, randomVolume / 2f));
-        source.pitch = pitch *(1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
+        source.pitch = pitch * (1 + Random.Range(-randomPitch / 2f, randomPitch / 2f));
         source.Play();
     }
-
 }
+
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
+
     [SerializeField]
     Sound[] sounds;
 
     void Awake()
     {
         if (instance != null)
-        instance = this;
+            instance = this;
     }
 
-    void Start ()
+    void Start()
     {
         for (int i = 0; i < sounds.Length; i++)
         {
-            for (int j = 0; j < sounds[i].clip.Length; j++)
+            for (var j = 0; j < this.sounds[i].clip.Length; j++)
             {
-                GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].clip[j]);
+                GameObject _go = new GameObject("Sound_" + i + "_" + this.sounds[i].clip[j]);
                 _go.transform.SetParent(this.transform);
-                sounds[i].SetSource(_go.AddComponent<AudioSource>());
+                this.sounds[i].SetSource(_go.AddComponent<AudioSource>(), j);
             }
-
-        
         }
-                
     }
 
 
-    public void PlaySound (int Sound)
+    public void PlaySound(int Sound)
     {
         for (int i = 0; i < sounds.Length; i++)
         {
             if (i == Sound)
             {
-                RandomAudioClip (i);
-                return;
+                RandomAudioClip(i);
+                break;
             }
         }
+
         // no sound with _name
         Debug.LogWarning("AudioManager: Sound not found in list (I canne´ focki´n find eit!): " + Sound);
     }
 
     private void RandomAudioClip(int i)
     {
-        var random = Random.Range(0, sounds.Length-1 );
-        AudioSource audio = GetComponentInParent<AudioSource>();
-        //audio.clip = transform.GetChild(random).GetComponent<AudioSource>();
-        audio.clip = sounds[i].clip[random];
+        var random = Random.Range(0, sounds.Length + 1);
+        AudioSource audio = transform.GetChild(random).GetComponent<AudioSource>();
+        //print(audio.transform.parent.name);
+        //audio.clip = transform.GetChild(0).GetComponent<AudioClip>();
+        //audio.clip = sounds[i].clip[random];
         audio.Play();
-        Debug.Log($"Playing sound: {sounds[random].name}");
+        Debug.Log($"Playing sound: {random}");
     }
 }
