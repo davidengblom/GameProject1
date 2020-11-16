29f9264a62f79edb18d6d1e14e135a399befddd0
Alex <alex.rynkowski90@gmail.com>
2020-11-16 12:57:24 +0100

    adds employeetype enum

---
+++ Project 1/Assets/Scripts/Upgrades/Character/EmployeeType.cs
@@ -0,0 +1,9 @@
+namespace Upgrades.Character
+{
+    public enum EmployeeType
+    {
+        Miner,
+        Hunter,
+        Lumberjack
+    }
+}
---
+++ Project 1/Assets/Scripts/Upgrades/Character/Experience.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+
+namespace Upgrades.Character
+{
+    [Serializable]
+    public class Experience
+    {
+        public int incrementFactorPerLevel = 2;
+        
+        
+        public EmployeeType employeeType;
+
+        int _currentLevel;
+        int _experienceToNextLevel;
+        public int ExperiencePoints
+        {
+            get => PlayerPrefs.GetInt("CurrentExperience" + this.employeeType, 0);
+            set => PlayerPrefs.SetInt("CurrentExperience" + this.employeeType, value);
+        }
+
+        public int ExperienceToNextLevel
+        {
+            get => this._experienceToNextLevel;
+            set => this._experienceToNextLevel = value;
+        }
+
+        public int CurrentLevel
+        {
+            get => PlayerPrefs.GetInt("CurrentLevel" + this.employeeType, 0);
+            set => this._currentLevel = value;
+        }
+    }
+}
