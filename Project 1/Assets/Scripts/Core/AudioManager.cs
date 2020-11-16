19a0d4a261af92ff2e8a0ac78cc58396bbda9904
AlchemistWolf <tobias_lundin@yahoo.com>
2020-11-16 18:11:07 +0100

    TRYING TO MAKE THE AUDIO WURK
    qq

--- Project 1/Assets/Scripts/Core/AudioManager.cs
+++ Project 1/Assets/Scripts/Core/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Sound
 {
     public string name;
-    public AudioClip clip;
+    public AudioClip [] clip;
 
     [Range(0f, 1f)]
     public float volume = 0.7f;
@@ -23,7 +23,7 @@ public class Sound
     public void SetSource (AudioSource _source)
     {
         source = _source;
-        source.clip = clip;
+        source.clip = clip[0];
     }
 
     public void Play ()
@@ -50,31 +50,40 @@ public class AudioManager : MonoBehaviour
     {
         for (int i = 0; i < sounds.Length; i++)
         {
-            GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].name);
-            _go.transform.SetParent(this.transform);
-            sounds[i].SetSource (_go.AddComponent<AudioSource>());
-        }
+            for (int j = 0; j < sounds[i].clip.Length; j++)
+            {
+                GameObject _go = new GameObject("Sound_" + i + "_" + sounds[i].clip[j]);
+                _go.transform.SetParent(this.transform);
+                sounds[i].SetSource(_go.AddComponent<AudioSource>());
+            }
 
         
-
+        }
+                
     }
 
 
-
-
-    public void PlaySound (string _name)
+    public void PlaySound (int Sound)
     {
         for (int i = 0; i < sounds.Length; i++)
         {
-            if (sounds[i].name == _name)
+            if (i == Sound)
             {
-                sounds[i].Play();
+                RandomAudioClip (i);
                 return;
             }
         }
         // no sound with _name
-        Debug.LogWarning("AudioManager: Sound not found in list (I canne´ focki´n find eit!): " + _name);
+        Debug.LogWarning("AudioManager: Sound not found in list (I canne´ focki´n find eit!): " + Sound);
     }
 
-
+    private void RandomAudioClip(int i)
+    {
+        var random = Random.Range(0, sounds.Length-1 );
+        AudioSource audio = GetComponentInParent<AudioSource>();
+        //audio.clip = transform.GetChild(random).GetComponent<AudioSource>();
+        audio.clip = sounds[i].clip[random];
+        audio.Play();
+        Debug.Log($"Playing sound: {sounds[random].name}");
+    }
 }
