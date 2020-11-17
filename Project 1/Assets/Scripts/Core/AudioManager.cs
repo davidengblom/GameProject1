9eb06c0d533cd19cc8726aaa458ea5bf99ce0ffe
AlchemistWolf <tobias_lundin@yahoo.com>
2020-11-17 13:36:48 +0100

    smal uppdates

--- Project 1/Assets/Scripts/Core/AudioManager.cs
+++ Project 1/Assets/Scripts/Core/AudioManager.cs
@@ -56,6 +56,8 @@ public class AudioManager : MonoBehaviour
                 this.sounds[i].SetSource(_go.AddComponent<AudioSource>(), j);
             }
         }
+
+       // PlaySound("Music");
     }
 
 
