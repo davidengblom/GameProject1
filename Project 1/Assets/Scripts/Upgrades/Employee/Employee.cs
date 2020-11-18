f1fd53348d4a9ef2a592373434b36f1f51dd6c7f
Alex <alex.rynkowski90@gmail.com>
2020-11-18 13:32:44 +0100

    fixes auto exp gain

--- Project 1/Assets/Scripts/Upgrades/Employee/Employee.cs
+++ Project 1/Assets/Scripts/Upgrades/Employee/Employee.cs
@@ -11,13 +11,7 @@ namespace Upgrades.Employee
         public float productionTime;
         [HideInInspector] public float timer;
         public Resource resource;
-        
-        public bool ShouldProduce => Time.time - this.timer > this.productionTime;
 
-        public int EmployeeLevel
-        {
-            get => PlayerPrefs.GetInt("EmployeeLevel" + this.type, 0);
-            set => PlayerPrefs.SetInt("EmployeeLevel" + this.type, value);
-        }
+        public bool ShouldProduce => Time.time - this.timer > this.productionTime;
     }
 }
