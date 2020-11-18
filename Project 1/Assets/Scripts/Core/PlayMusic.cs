fdb6d40fb3656a2d59214621ba090547284e4c47
Alex <alex.rynkowski90@gmail.com>
2020-11-18 16:38:38 +0100

    adds volume settings for player

---
+++ Project 1/Assets/Scripts/Core/PlayMusic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayMusic : MonoBehaviour
+{
+    Slider _slider;
+    public AudioSource _audioSource;
+
+    void Start()
+    {
+        this._slider = GetComponent<Slider>();
+        this._slider.value = SetMusicVolume;
+        VolumeSetter();
+    }
+
+    void Update()
+    {
+        VolumeSetter();
+    }
+
+    public float SetMusicVolume
+    {
+        get => PlayerPrefs.GetFloat("MusicVolume", .1f);
+        set => PlayerPrefs.SetFloat("MusicVolume", value);
+    }
+
+    void VolumeSetter()
+    {
+        this._audioSource.volume = SetMusicVolume;
+    }
+}
