177ee72c95a93cdb6f3de89e4ab3caea68b447f9
Alex <alex.rynkowski90@gmail.com>
2020-11-19 00:08:53 +0100

    adds a script that makes popup text when hovering over a gameobject

---
+++ Project 1/Assets/Scripts/HUD/HoverOverDescription.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverOverDescription : MonoBehaviour, IEventSystemHandler
+{
+    public GameObject description;
+
+    void Update()
+    {
+        var pointer = new PointerEventData(EventSystem.current) {position = Input.mousePosition};
+
+        var rayCastResult = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(pointer, rayCastResult);
+
+        if (rayCastResult.Count > 0)
+        {
+            foreach (var result in rayCastResult)
+            {
+                if (result.gameObject.name == this.gameObject.name)
+                {
+                    this.description.SetActive(true);
+                    return;
+                }
+            }
+        }
+
+        this.description.SetActive(false);
+    }
+}
