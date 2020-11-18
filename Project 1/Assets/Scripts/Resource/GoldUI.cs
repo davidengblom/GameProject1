6a8dc9a43c95bd385c3a61ffcd69c4dcacdbb583
Alex <alex.rynkowski90@gmail.com>
2020-11-18 14:35:03 +0100

    Implements IReset interface on ResourceUI and GoldUI

--- Project 1/Assets/Scripts/Resource/GoldUI.cs
+++ Project 1/Assets/Scripts/Resource/GoldUI.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class GoldUI : MonoBehaviour
+public class GoldUI : MonoBehaviour, IReset
 {
     public Resource gold;
     public Text amountText;
+
     void Update()
     {
         amountText.text = $"Gold: {gold.Owned}";
+    }
+
+    public void Reset()
+    {
+        this.gold.Owned = 0;
     }
 }
