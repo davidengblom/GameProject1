3fac6ba639fef3f4ad053bc03218ca423f77d151
Alex <alex.rynkowski90@gmail.com>
2020-11-19 10:38:44 +0100

    fixes core script folder

---
+++ Project 1/Assets/Scripts/Core/Fader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Core
+{
+    public class Fader : MonoBehaviour
+    {
+        CanvasGroup _canvasGroup;
+
+        void Start()
+        {
+            this._canvasGroup = GetComponent<CanvasGroup>();
+            ActivateCanvasGroup(false);
+        }
+
+        
+
+        public IEnumerator FadeOut()
+        {
+            this._canvasGroup = GetComponent<CanvasGroup>();
+            ActivateCanvasGroup(true);
+            var timer = 0f;
+
+            while (timer < 1)
+            {
+                timer += Time.deltaTime;
+                this._canvasGroup.alpha += Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        public IEnumerator FadeIn()
+        {
+            this._canvasGroup = GetComponent<CanvasGroup>();
+            var timer = 1f;
+
+            while (timer > 0)
+            {
+                timer -= Time.deltaTime;
+                this._canvasGroup.alpha -= Time.deltaTime;
+                yield return null;
+            }
+
+            ActivateCanvasGroup(false);
+        }
+
+        void ActivateCanvasGroup(bool shouldShow)
+        {
+            if (shouldShow)
+            {
+                this._canvasGroup.interactable = true;
+                this._canvasGroup.blocksRaycasts = true;
+            }
+            else
+            {
+                this._canvasGroup.interactable = false;
+                this._canvasGroup.blocksRaycasts = false;
+            }
+        }
+    }
+}
---
+++ Project 1/Assets/Scripts/Core/PersistantObjects.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistantObjects : MonoBehaviour
+{
+    PersistantObjects instance;
+
+    void Start()
+    {
+        this.instance = FindObjectOfType<PersistantObjects>();
+
+        DontDestroyOnLoad(this.instance.gameObject);
+    }
+}
