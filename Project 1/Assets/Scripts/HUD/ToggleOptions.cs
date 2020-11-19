bce3074dc8802d659dc54d2f59c23a4909516de2
Alex <alex.rynkowski90@gmail.com>
2020-11-19 11:37:14 +0100

    adds toggle optionsmenu scripts

---
+++ Project 1/Assets/Scripts/HUD/ToggleOptions.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleOptions : MonoBehaviour
+{
+    public bool optionsMenuShowing = false;
+
+    public void OpenOptionsMenu()
+    {
+        ShowCanvasGroup(true);
+    }
+
+
+    public void CloseOptionsMenu()
+    {
+        ShowCanvasGroup(false);
+    }
+
+    void ShowCanvasGroup(bool shouldShow)
+    {
+        var canvasGroup = GetComponent<CanvasGroup>();
+        if (shouldShow)
+        {
+            canvasGroup.alpha = 1;
+            canvasGroup.interactable = true;
+            canvasGroup.blocksRaycasts = true;
+            this.optionsMenuShowing = true;
+        }
+        else
+        {
+            canvasGroup.alpha = 0;
+            canvasGroup.interactable = false;
+            canvasGroup.blocksRaycasts = false;
+            this.optionsMenuShowing = false;
+        }
+    }
+}
