bf09624f0959fe9c2651894b7518900c990d6f53
Apollotwin <73161942+Apollotwin@users.noreply.github.com>
2020-11-19 09:35:21 +0100

    Started working on timer

---
+++ Project 1/Assets/Scripts/Upgrades/Market/TimerDailyCap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerDailyCap : MonoBehaviour
+{
+    public Market dailyCap;
+    public Text timerText;
+    public float countDownTime = 24;
+
+    private float timer;
+
+    private void Update()
+    {
+        
+    }
+
+    void FormatText()
+    {
+        int hour = (int)(timer / 3600) % 24;
+        int minute = (int)(timer / 60) % 60;
+        int second = (int) (timer / 60);
+
+        if (hour > 0) { timerText.text += $"{hour}:"; }
+        if (minute > 0) { timerText.text += minute; }
+        if (second > 0) { timerText.text += second; }
+    }
+    
+    public float SaveTime
+    {
+        get => PlayerPrefs.GetFloat("Timer", 0);
+        set => PlayerPrefs.SetFloat("Timer", Mathf.Clamp(value, 0,countDownTime));
+    }
+}
