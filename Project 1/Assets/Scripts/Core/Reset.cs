df1c7fbdd4c24735622c7351b149262cc0970a42
Alex <alex.rynkowski90@gmail.com>
2020-11-19 21:15:51 +0100

    fixes bugs using bugspray

--- Project 1/Assets/Scripts/Core/Reset.cs
+++ Project 1/Assets/Scripts/Core/Reset.cs
@@ -1,10 +1,14 @@
 using System;
+using Achievements;
+using HUD;
 using UnityEngine;
 
 namespace Core
 {
     public class Reset : MonoBehaviour
     {
+        public Resource _resource;
+
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.A))
@@ -15,6 +19,8 @@ namespace Core
 
         void ResetGame()
         {
+            this._resource.Owned += (int) (CalculateTotalLevel() * .2f);
+            
             foreach (var monoBehaviour in FindObjectsOfType<MonoBehaviour>())
             {
                 if (monoBehaviour is IReset res)
@@ -22,6 +28,17 @@ namespace Core
                     res.Reset();
                 }
             }
+        }
+
+        float CalculateTotalLevel()
+        {
+            var totalLevel = 0f;
+            foreach (var reference in FindObjectsOfType<AchievementReference>())
+            {
+                totalLevel += reference.GetComponent<ExperienceUI>()._experience.CurrentLevel;
+            }
+
+            return totalLevel;
         }
     }
 }
