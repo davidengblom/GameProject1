cfc11448ae823f0e66b67a4311e815a5664a104a
Apollotwin <73161942+Apollotwin@users.noreply.github.com>
2020-11-19 13:13:46 +0100

    Copied MainScene to Simon scene =)

--- Project 1/Assets/Scripts/Upgrades/Character/CharacterLevel.cs
+++ Project 1/Assets/Scripts/Upgrades/Character/CharacterLevel.cs
@@ -1,6 +1,25 @@
-﻿using UnityEngine;
+﻿using System;
+using Achievements;
+using HUD;
+using UnityEngine;
 
 public class CharacterLevel : MonoBehaviour
 {
     
+    private void Update()
+    {
+        CalculateTotalLevel();
+    }
+
+    public float CalculateTotalLevel()
+    {
+        var totalLevel = 0;
+
+        foreach (var level in FindObjectsOfType<AchievementReference>())
+        {
+            totalLevel += level.GetComponent<ExperienceUI>()._experience.CurrentLevel;
+        }
+
+        return totalLevel;
+    }
 }
